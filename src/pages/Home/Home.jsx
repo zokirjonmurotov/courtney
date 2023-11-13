96ba96016ae92c80b6ca7d8eff9b03a2a2f2c6dd
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-11-13 19:29:33 +0500

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const HomeCardData = [
+    {
+
+    }
+]
+
+const Home = () => {
+    return (
+        <div>
+
+            {/* <Button /> */}
+            {/* <Showcase /> */}
+            {/* {
+                HomeCardData.map((item)=>(
+                    <Card ti/>
+                ))
+            } */}
+        </div>
+    );
+}
+
+export default Home;
