9d2b5c34a3bdbae7bb635a4ac467b5f0b59225eb
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-15 15:41:41 +0500

    added clients section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Specialized from './pages/Home/components/specialized/Specialized'
 
 function App() {
   
 
   return (
     <>
-     <Specialized/>
+    
     </>
   )
 }
