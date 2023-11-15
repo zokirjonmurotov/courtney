6c69e3bb21105ab3e0f2dc2558d1d2870718ef3a
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-15 18:34:36 +0500

    home page is done

--- src/pages/Home/components/expertise/Expertise.jsx
+++ src/pages/Home/components/expertise/Expertise.jsx
@@ -13,10 +13,10 @@ export default function Expertise () {
             <Container>
             <HomeTitle h4='Expertise' h2='Hire Permanent and Remote Developers' p='From full-time remote engineering teams to hourly contractors, work with remote devs as needed' left='left'/>
             <div className={styles.expertise}>
-            <GlobalCard width='23%' icon={icon1} heading='Dedicated Teams' bottom='Find your next team member' />
-            <GlobalCard width='23%' icon={icon2} heading='Staff Augmentation' bottom='Build a distributed development team' />
-            <GlobalCard width='23%' icon={icon3} heading='Software Outsourcing' bottom='End-to-End Software Development Outsourcing Solutions' />
-            <GlobalCard width='23%' icon={icon4} heading='Remote Office' bottom='Open your own remote development center and grow your business' />
+            <GlobalCard width='100%' icon={icon1} heading='Dedicated Teams' bottom='Find your next team member' />
+            <GlobalCard width='100%' icon={icon2} heading='Staff Augmentation' bottom='Build a distributed development team' />
+            <GlobalCard width='100%' icon={icon3} heading='Software Outsourcing' bottom='End-to-End Software Development Outsourcing Solutions' />
+            <GlobalCard width='100%' icon={icon4} heading='Remote Office' bottom='Open your own remote development center and grow your business' />
             </div>
         </Container>
         </div>
