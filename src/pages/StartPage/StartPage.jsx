b237842f99c3bba4f7237a7d5a3103e031760cac
Lola <lolabotiralievna@gmail.com>
2023-11-20 18:32:23 +0500

    the position of apply is changed

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Showcase from '../../Components/Showcase';
 import Start from './components/Start/Start';
 import DevSection from './components/DevSection/DevSection'
-import Apply from '../../Components/Apply/Apply';
 const StartPage = () => {
     return (
         <div>
-            <Showcase/>
+            <Showcase showcaseBigText='How to Start' showcaseSmallText='We specialize in helping you build a team of expert developers.'/>
             <Start/>
             <DevSection/>
-            <Apply/>
         </div>
     );
 }
